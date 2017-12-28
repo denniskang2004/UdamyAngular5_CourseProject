e5c177db56374f25589fc2d89f96c58942a1310b
denniskang2004 <denniskang.dev@gmail.com>
2017-12-27 19:15:58 -0600

    lecture 77: added routing by using @Output&ngIf

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Recipe} from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,11 +8,16 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None // dknote 67: change to apply css globally
 })
 export class RecipesComponent implements OnInit {
-
+  recipeSelected:Recipe;
   constructor() {
+
   }
 
   ngOnInit() {
   }
 
+  rcpSelectItem(itemSelected:Recipe){
+    this.recipeSelected = itemSelected;
+    console.log("selected item name:"+this.recipeSelected.name);
+  }
 }
