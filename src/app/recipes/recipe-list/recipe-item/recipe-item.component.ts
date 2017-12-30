c5b70a1266b5d453ed99f6355c9ba47a699d3971
denniskang2004 <denniskang.dev@gmail.com>
2017-12-30 15:09:05 -0600

    lecture 148/149/150:Project: set up children recipe routing by parameters

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Recipe} from '../../recipe.model';
 import {RecipesService} from '../../recipes.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,18 +9,23 @@ import {RecipesService} from '../../recipes.service';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  @Input() itemIndex:number;
   @Input() recipe: Recipe;
   // @Output() itemSelected = new EventEmitter<Recipe>(); // dknote 106: change to use Recipe Service
 
 
-  constructor(private recipeService:RecipesService) {}
+  constructor(
+    private recipeService:RecipesService,
+    private router:Router
+  ) {}
 
   ngOnInit() {
   }
 
-  onClickItem(item: Recipe) {
-    // this.itemSelected.emit(item); // dknote 106: change to use Recipe Service
-    this.recipeService.recipeSelected.emit(item);
-  }
+  // dknote 149, dknote 150: use router parameter
+  // onClickItem(item: Recipe) {
+  //   // this.itemSelected.emit(item); // dknote 106: change to use Recipe Service
+  //   // this.recipeService.recipeSelected.emit(item); dknote: change to use router param
+  //
+  // }
 }
