02d3f042360bed0a2ad30c2ea8cf9c42819675a2
denniskang2004 <denniskang.dev@gmail.com>
2018-01-01 08:45:07 -0600

    lecture 195: Reactive form: Custom Validator

--- src/app/server/form-demo-reactive/form-demo-reactive.component.ts
+++ src/app/server/form-demo-reactive/form-demo-reactive.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -10,9 +10,11 @@ export class FormDemoReactiveComponent implements OnInit {
 
   // dknote 187: set up reactive form
   genders = ['male', 'female'];
-  signupForm:FormGroup;
+  signupForm: FormGroup;
+  forbiddenNames = ['Yuan', 'Dennis'];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     // dknote 188: create / add controls to reactive forms
@@ -20,26 +22,38 @@ export class FormDemoReactiveComponent implements OnInit {
       'userData': new FormGroup({
         'username': new FormControl(
           null,
-          Validators.required // dknote: add validation
+          [Validators.required,// dknote: add validation
+            this.validateForbiddenName.bind(this)]  //dknote 195: custom validator
         ),
-        'email' : new FormControl(
+        'email': new FormControl(
           null,
-          [Validators.required,Validators.email]//dknote: add validation
+          [Validators.required, Validators.email]//dknote: add validation
         )
       }),
-      'gender' : new FormControl('male'),
-      'hobbies' : new FormArray([]) //dknote: add form array as container for dynamically created form controls
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([]) //dknote: add form array as container for dynamically created form controls
 
     });
 
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupForm);
   }
 
-  onAddHobby(){
-    (<FormArray>this.signupForm.get('hobbies')).push(new FormControl(null,Validators.required));
+  onAddHobby() {
+    (<FormArray>this.signupForm.get('hobbies')).push(new FormControl(null, Validators.required));
   }
 
+  // dknote 195: custom validator
+  validateForbiddenName(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenNames.indexOf(control.value) !== -1) {
+      return {
+        'nameIsForbidden': true
+      };
+    }
+    else {
+      return null; // important: return null, not false
+    }
+  }
 }
