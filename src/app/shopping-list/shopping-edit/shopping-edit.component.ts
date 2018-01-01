2776dfde59835a380ee81d061fbcde0e538b24de
denniskang2004 <denniskang.dev@gmail.com>
2018-01-01 09:45:54 -0600

    lecture 201/202: Project: set up template-driven form (setup + validation)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild, Output, EventEmitter,ElementRef} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,8 +10,8 @@ import {ShoppingListService} from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
   // @Output() itemAdded = new EventEmitter<Ingredient>(); // dknote 80: pass data out // dknote 106: use shopping list service
-  @ViewChild('inputName') inputName:ElementRef; // dknote 80: use VeiwChild
-  @ViewChild('inputAmount') inputAmount:ElementRef;
+  // @ViewChild('inputName') inputName:ElementRef; // dknote 80: use VeiwChild
+  // @ViewChild('inputAmount') inputAmount:ElementRef;
 
   // dknote 108: use shopping list service
   constructor(private shopService:ShoppingListService) {  }
@@ -19,14 +20,18 @@ export class ShoppingEditComponent implements OnInit {
   }
 
   // dknote 80: use the ViewChild
-  onAddItem(){
+  onAddItem(form:NgForm){
     // dknote 108: use shopping list service
     // this.itemAdded.emit(
     //   new Ingredient(this.inputName.nativeElement.value,this.inputAmount.nativeElement.value)
     // );
 
-    let newIngredient = new Ingredient(this.inputName.nativeElement.value,this.inputAmount.nativeElement.value);
-    this.shopService.addShoppingItem(newIngredient);
+    // let newIngredient = new Ingredient(this.inputName.nativeElement.value,this.inputAmount.nativeElement.value);
+    // this.shopService.addShoppingItem(newIngredient);
+
+    // dknote 201/202: change to use template driven form:
+     let newIngredient = new Ingredient(form.value.name, form.value.amount);
+     this.shopService.addShoppingItem(newIngredient);
 
   }
 }
