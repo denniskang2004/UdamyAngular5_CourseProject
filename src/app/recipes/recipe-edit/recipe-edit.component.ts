2b473d941323521565a4156d2be61b71ad824564
denniskang2004 <denniskang.dev@gmail.com>
2018-01-01 11:16:11 -0600

    lecture 210/211: Project: Setup/Sync for Recipe Edit Form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
+import {RecipesService} from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,17 +11,41 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id:number;
   isNew:boolean;
+  editForm: FormGroup;
+  editMode:boolean;
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(
+    private route:ActivatedRoute,
+    private recipeService:RecipesService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (param:Params)=>{
         this.id = param['id'];
         this.isNew = this.id==null;
-
+        this.editMode = this.id!==null;
+        this.initForm();
       }
     )
+  }
+
+  initForm(){
+    let name = '';
+    let description = '';
+    let imagePath = '';
+
+    if(this.editMode){
+      let recipe = this.recipeService.getRecipe(this.id);
+      name = recipe.name;
+      description = recipe.description;
+      imagePath = recipe.imagePath;
+    }
+
+    this.editForm = new FormGroup({
+      'name': new FormControl(name),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(description)
+    });
   }
 
 }
