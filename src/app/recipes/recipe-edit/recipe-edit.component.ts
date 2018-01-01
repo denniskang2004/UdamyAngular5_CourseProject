2492817c58216ddbd0a6168e5724efca2ad4daa0
denniskang2004 <denniskang.dev@gmail.com>
2018-01-01 12:11:32 -0600

    lecture 212: Project: Sync Recipes' Edit ingredients with FormArray

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 import {RecipesService} from '../recipes.service';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -13,6 +14,7 @@ export class RecipeEditComponent implements OnInit {
   isNew:boolean;
   editForm: FormGroup;
   editMode:boolean;
+
 
   constructor(
     private route:ActivatedRoute,
@@ -33,18 +35,31 @@ export class RecipeEditComponent implements OnInit {
     let name = '';
     let description = '';
     let imagePath = '';
+    let ingredents = new FormArray([]);
 
     if(this.editMode){
       let recipe = this.recipeService.getRecipe(this.id);
       name = recipe.name;
       description = recipe.description;
       imagePath = recipe.imagePath;
+
+      // dknote: populate FormArray from existing ingredients of a recipe
+      if(recipe.ingredients){
+        for(let ing of recipe.ingredients){
+          ingredents.push(new FormGroup({
+            name:new FormControl(ing.name),
+            amount:new FormControl(ing.amount)
+          }))
+        }
+      }
+
     }
 
     this.editForm = new FormGroup({
       'name': new FormControl(name),
       'imagePath': new FormControl(imagePath),
-      'description': new FormControl(description)
+      'description': new FormControl(description),
+      'ingredients': ingredents
     });
   }
 
